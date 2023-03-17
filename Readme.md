c78a9612220c78363e201f5b7918ebd770a162d6
brrruski <sizhe4real@gmail.com>
2023-03-17 10:36:39 -0400

    init template

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Tiny React
+
+Let's re-implement a tiny but working version of React!
+
+Follow the ordered tags to learn how it is built step by step.
---
+++ src/index.js
@@ -0,0 +1,18 @@
+// render these jsx onto the html page!
+const jsx = (
+  <div className="container">
+    <h1>Hi, Tiny React</h1>
+    <h2 data-test="test">(H2 with data attribute)</h2>
+    <div>
+      Embeded 1 <div>Embeded 1.1</div>
+    </div>
+    <h3>(Watch out: this would change)</h3>
+    {2 == 1 && <div>if the statement is true, this would show</div>}
+    {2 == 2 && <div>2</div>}
+    <span>Something trivial</span>
+    <button onClick={() => alert("Hello world!")}>Click me</button>
+    <h3>This would be removed</h3>
+    2, 3
+    <input type="text" value="13" />
+  </div>
+);
